32d8a513427001291ad89247a23ed7aab9523eed
Satyajit <iamsatwa@gmail.com>
2021-05-13 19:47:33 +0530

    fixed

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
-# from app import app
-# from db import db
-#
-# db.init_app(app)
-#
-#
-# @app.before_first_request  # Registers a function to be run before the first request to this instance of the application
-# def create_tables():
-#     db.create_all()
+from app import app
+from db import db
+
+db.init_app(app)
+
+
+@app.before_first_request  # Registers a function to be run before the first request to this instance of the application
+def create_tables():
+    db.create_all()
